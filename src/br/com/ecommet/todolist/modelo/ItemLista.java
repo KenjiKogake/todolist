1e1773ac56e6c2bdc9cf6841479f5c8b65c6ef66
KenjiKogake <kenji.kogake@gmail.com>
2016-09-10 12:39:25 -0300

    Adiciona mapeamento das entidades

--- src/br/com/ecommet/todolist/modelo/ItemLista.java
+++ src/br/com/ecommet/todolist/modelo/ItemLista.java
@@ -1,28 +1,59 @@
 package br.com.ecommet.todolist.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class ItemLista {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(length = 100)
 	private String descricao;
+
 	private boolean feito;
+
+	@ManyToOne
+	@JoinColumn(name="id_lista")
+	private Lista lista;
 
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public boolean isFeito() {
 		return feito;
 	}
+
 	public void setFeito(boolean feito) {
 		this.feito = feito;
 	}
-	
+
+	public Lista getLista() {
+		return lista;
+	}
+
+	public void setLista(Lista lista) {
+		this.lista = lista;
+	}
+
 }
