cc2a8cad94fe8775682a4245cf257b523a7e7c08
KenjiKogake <kenji.kogake@gmail.com>
2016-09-10 11:46:11 -0300

    Teste

--- src/br/com/ecommet/todolist/modelo/Lista.java
+++ src/br/com/ecommet/todolist/modelo/Lista.java
@@ -1,5 +1,30 @@
 package br.com.ecommet.todolist.modelo;
 
-public class Lista {
+import java.util.List;
 
+public class Lista {
+	
+	private Long id;
+	private String titulo;
+	private List<ItemLista> itens;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public List<ItemLista> getItens() {
+		return itens;
+	}
+	public void setItens(List<ItemLista> itens) {
+		this.itens = itens;
+	}
+	
 }
